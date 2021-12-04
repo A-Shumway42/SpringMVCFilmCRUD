97270a1dfab2c6540ad8ba49c69bc93bc98584d4
Gramman87 <gramman87@gmail.com>
2021-12-04 10:47:34 -0700

    intermittent frontend push

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,4 +1,5 @@
 package com.skilldistillery.film.controllers;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,14 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.skilldistillery.film.data.FilmDAO;
 
-@Controller 
+@Controller
 public class FilmController {
-	
-	
+
 	@Autowired
 	private FilmDAO filmDao;
-	
-	@RequestMapping({"/", "home.do"})
+
+	@RequestMapping({ "/", "home.do" })
 	public String home(Model model) {
 		model.addAttribute("TEST", "Hello, Spring MVC!");
 		return "home";
